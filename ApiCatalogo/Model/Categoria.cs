77a42ae070e5250b64ec6c0a0676696fc43038d4
saldanha <darkw2078@gmail.com>
2025-03-26 11:28:05 -0300

    commit inicial para API Catalogo

---
+++ ApiCatalogo/Model/Categoria.cs
@@ -0,0 +1,11 @@
+namespace ApiCatalogo.Model
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string? Nome { get; set; }
+        public string? ImagemUrl { get; set; }
+
+        public ICollection<Produto> Produtos { get; set; }
+    }
+}
---
+++ ApiCatalogo/Model/Produto.cs
@@ -0,0 +1,16 @@
+namespace ApiCatalogo.Model
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string? ImagemUrl { get; set; }
+        public DateTime DataCadastro { get; set; }
+
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+
+    }
+}
