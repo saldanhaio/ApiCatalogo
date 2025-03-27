15b2e6121de5cc469657f5937815abb6f617260a
saldanha <darkw2078@gmail.com>
2025-03-26 22:20:32 -0300

    aplicando ajuste de migration

--- ApiCatalogo/Model/Categoria.cs
+++ ApiCatalogo/Model/Categoria.cs
@@ -1,9 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ApiCatalogo.Model
 {
+    [Table("Categorias")]
     public class Categoria
     {
+        [Key]
         public int CategoriaId { get; set; }
+
+        [Required]
+        [StringLength(80)]
         public string? Nome { get; set; }
+
+        [Required]
+        [StringLength(300)]
         public string? ImagemUrl { get; set; }
 
         public ICollection<Produto> Produtos { get; set; }
--- ApiCatalogo/Model/Produto.cs
+++ ApiCatalogo/Model/Produto.cs
@@ -1,11 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ApiCatalogo.Model
 {
+    [Table("Produtos")]
     public class Produto
     {
+        [Key]
         public int ProdutoId { get; set; }
+
+        [Required]
+        [StringLength(80)]
         public string? Nome { get; set; }
+
+        [Required]
+        [StringLength(300)]
         public string? Descricao { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(8, 2)")]
         public decimal Preco { get; set; }
+
+        [Required]
+        [StringLength(300)]
         public string? ImagemUrl { get; set; }
         public DateTime DataCadastro { get; set; }
 
