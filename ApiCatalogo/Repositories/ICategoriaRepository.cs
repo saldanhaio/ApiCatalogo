5bd6d8ff8549ac04210cabfa705b2dc9f19160bb
saldanhaio <darkw2078@gmail.com>
2025-04-29 15:30:23 -0300

    correcao

---
+++ ApiCatalogo/Repositories/ICategoriaRepository.cs
@@ -0,0 +1,15 @@
+using ApiCatalogo.Model;
+
+namespace ApiCatalogo.Repositories
+{
+    public interface ICategoriaRepository
+    {
+    IEnumerable<Categoria> GetCategorias();
+      Categoria GetCategoria(int id);
+      Categoria Create(Categoria categoria);
+     Categoria Update(Categoria categoria);
+
+      Categoria Delete(int id);
+
+    }
+}
